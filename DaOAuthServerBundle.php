c29bdb9586926a75d813ada46031854acb7c0582
Thomas <thomas.prelot@tessi.fr>
2013-07-30 15:24:28 +0200

    Alias bundle name correction

--- DaOAuthServerBundle.php
+++ DaOAuthServerBundle.php
@@ -3,7 +3,15 @@
 namespace Da\OAuthServerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Acme\HelloBundle\DependencyInjection\UnconventionalExtensionClass;
 
 class DaOAuthServerBundle extends Bundle
 {
+	public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        // register extensions that do not follow the conventions manually
+        $container->registerExtension(new UnconventionalExtensionClass());
+    }
 }
