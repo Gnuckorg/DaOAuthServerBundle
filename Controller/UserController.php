82eb4fdb2dc26a94d6233fe1cb85ac1716d9d929
Gnucki <tprelot@gmail.com>
2014-11-24 12:38:28 +0100

    Add users API

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -7,7 +7,6 @@ use FOS\RestBundle\Routing\ClassResourceInterface;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\RequestParam;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
-use FOS\RestBundle\Controller\Annotations\Put;
 
 class UserController extends FOSRestController implements ClassResourceInterface
 {
@@ -38,38 +37,12 @@ class UserController extends FOSRestController implements ClassResourceInterface
     )
     {
         try {
-            $enabled = (Boolean) $enabled;
-
-            if (null === $raw || empty($raw)) {
-                $raw = array();
-            } else {
-                $raw = json_decode($raw, true);
-
-                if (null === $raw || !is_array($raw)) {
-                    throw new \InvalidArgumentException('The raw parameter must be a JSON string.');
-                }
-            }
-
             $request = $this->container->get('request');
             $userManager = $this->container->get('fos_user.user_manager');
-            $parameters = $request->request->all();
 
-            foreach ($parameters as $key => $value) {
-                if (!in_array(
-                    $key,
-                    array(
-                        'username',
-                        'email',
-                        'password',
-                        'raw',
-                        'enabled'
-                    )
-                )) {
-                    if (!isset($raw[$key])) {
-                        $raw[$key] = $value;
-                    }
-                }
-            }
+            $enabled = (Boolean) $enabled;
+            $raw = $this->formatRawData($request, $raw);
+            $authspace = $this->getClient($request)->getAuthSpace();
 
             $user = $userManager->createUser();
             $encoder = $this->container->get('security.encoder_factory')->getEncoder($user);
@@ -93,10 +66,8 @@ class UserController extends FOSRestController implements ClassResourceInterface
     }
 
     /**
-     * [PUT] /users/{username}
+     * [PUT] /users/{id}
      * Modify a user.
-     *
-     * @Put("/users")
      *
      * @RequestParam(name="username", strict=true, description="The username.")
      * @RequestParam(name="email", strict=true, description="The email.")
@@ -111,6 +82,7 @@ class UserController extends FOSRestController implements ClassResourceInterface
      * @param string $enabled  Enabled user?
      */
     public function putAction(
+        $id,
         $username,
         $email,
         $password,
@@ -119,45 +91,19 @@ class UserController extends FOSRestController implements ClassResourceInterface
     )
     {
         try {
-            $enabled = (Boolean) $enabled;
-
-            if (null === $raw || empty($raw)) {
-                $raw = array();
-            } else {
-                $raw = json_decode($raw, true);
-
-                if (null === $raw || !is_array($raw)) {
-                    throw new \InvalidArgumentException('The raw parameter must be a JSON string.');
-                }
-            }
-
             $request = $this->container->get('request');
             $userManager = $this->container->get('fos_user.user_manager');
-            $parameters = $request->request->all();
 
-            foreach ($parameters as $key => $value) {
-                if (!in_array(
-                    $key,
-                    array(
-                        'username',
-                        'email',
-                        'password',
-                        'raw',
-                        'enabled'
-                    )
-                )) {
-                    if (!isset($raw[$key])) {
-                        $raw[$key] = $value;
-                    }
-                }
-            }
-
+            $enabled = (Boolean) $enabled;
+            $raw = $this->formatRawData($request, $raw);
             $authspace = $this->getClient($request)->getAuthSpace();
-            $user = $userManager->findUserBy(array('username' => $username, 'authSpace' => $authspace->getId()));
+            //$user = $userManager->findUserBy(array('username' => $username, 'authSpace' => $authspace->getId()));
+            $user = $userManager->findUserBy(array('id' => $id));
 
             if (null === $user) {
                 throw new \LogicException(sprintf(
-                    'User "%s" not found in authspace "%s".',
+                    'User "%s"(%s) not found in authspace "%s".',
+                    $id,
                     $username,
                     $authspace->getId()
                 ));
@@ -186,7 +132,180 @@ class UserController extends FOSRestController implements ClassResourceInterface
     }
 
     /**
-     * @return ClientInterface
+     * [PATCH] /users/{id}
+     * Modify a part of a user.
+     *
+     * @RequestParam(name="username", strict=true, nullable=true, description="The username.")
+     * @RequestParam(name="email", strict=true, nullable=true, description="The email.")
+     * @RequestParam(name="password", strict=true, nullable=true, description="The password.")
+     * @RequestParam(name="raw", strict=true, nullable=true, description="The other data in a raw JSON.")
+     * @RequestParam(name="enabled", requirements="0|1", strict=true, nullable=true, description="Enabled user?")
+     *
+     * @param string $username The username.
+     * @param string $email    The email.
+     * @param string $password The password.
+     * @param string $raw      The other data in a raw JSON.
+     * @param string $enabled  Enabled user?
+     */
+    public function patchAction(
+        $id,
+        $username,
+        $email,
+        $password,
+        $raw,
+        $enabled
+    )
+    {
+        try {
+            $request = $this->container->get('request');
+            $userManager = $this->container->get('fos_user.user_manager');
+
+            $user = $userManager->findUserBy(array('id' => $id));
+            $enabled = (Boolean) $enabled;
+            $oldRaw = $user->getRawData();
+            if (null === $oldRaw) {
+                $oldRaw = array();
+            } else if (!is_array($oldRaw)) {
+                $oldRaw = array('content' => $oldRaw);
+            }
+            $raw = $this->formatRawData($request, $raw, $oldRaw);
+            $authspace = $this->getClient($request)->getAuthSpace();
+
+            if (null === $user) {
+                throw new \LogicException(sprintf(
+                    'User "%s" not found in authspace "%s".',
+                    $id,
+                    $authspace->getId()
+                ));
+            }
+
+            $encoder = $this->container->get('security.encoder_factory')->getEncoder($user);
+            if (null !== $password) {
+                $password = $encoder->encodePassword($password, $user->getSalt());
+                $user->setPassword($password);
+            }
+
+            if (null !== $username) {
+                $user->setUsername($username);
+            }
+            if (null !== $email) {
+                $user->setEmail($email);
+            }
+            if (null !== $raw) {
+                $user->setRawData($raw);
+            }
+            if (null !== $authspace) {
+                $user->setAuthSpace($authspace);
+            }
+            if (null !== $enabled) {
+                $user->setEnabled($enabled);
+            }
+
+            $userManager->updateUser($user);
+
+            $view = $this->view(array(), 204);
+        } catch (\LogicException $exception) {
+            $view = $this->view(array('error' => $exception->getMessage()), 404);
+        } catch (\Exception $exception) {
+            $view = $this->view(array('error' => $exception->getMessage()), 400);
+        }
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * [DELETE] /users/{id}
+     * Delete a user (disabling).
+     */
+    public function deleteAction($id)
+    {
+        try {
+            $userManager = $this->container->get('fos_user.user_manager');
+
+            $user = $userManager->findUserBy(array('id' => $id));
+
+            if (null === $user) {
+                throw new \LogicException(sprintf(
+                    'User "%s" not found in authspace "%s".',
+                    $id,
+                    $authspace->getId()
+                ));
+            }
+
+            $raw = $user->getRawData();
+            if (null === $raw || empty($raw)) {
+                $raw = array();
+            }
+            if (is_array($raw)) {
+                $now = new \DateTime();
+                $raw['deletedAt'] = $now->format(\DateTime::ISO8601);
+                $user->setRawData($raw);
+            }
+            $user->setEnabled(false);
+
+            $userManager->updateUser($user);
+
+            $view = $this->view(array(), 204);
+        } catch (\LogicException $exception) {
+            $view = $this->view(array('error' => $exception->getMessage()), 404);
+        } catch (\Exception $exception) {
+            $view = $this->view(array('error' => $exception->getMessage()), 400);
+        }
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * Format raw data.
+     *
+     * @param Request $request The request.
+     * @param mixed   $raw     The initial raw data.
+     * @param mixed   $oldRaw  The old raw data to merge.
+     *
+     * @return array The raw data.
+     *
+     * @throws \InvalidArgumentException if the passed raw has not a json format.
+     */
+    protected function formatRawData(Request $request, $raw, array $oldRaw = array())
+    {
+        if (null === $raw || empty($raw)) {
+            $raw = array();
+        } else {
+            $raw = json_decode($raw, true);
+
+            if (null === $raw || !is_array($raw)) {
+                throw new \InvalidArgumentException('The raw parameter must be a JSON string.');
+            }
+        }
+
+        foreach ($request->request->all() as $key => $value) {
+            if (!in_array(
+                $key,
+                array(
+                    'username',
+                    'email',
+                    'password',
+                    'raw',
+                    'enabled'
+                )
+            )) {
+                if (!isset($raw[$key])) {
+                    $raw[$key] = $value;
+                }
+            }
+        }
+
+        $raw = array_merge($oldRaw, $raw);
+
+        return $raw;
+    }
+
+    /**
+     * Get the current client.
+     *
+     * @param Request $request The request.
+     *
+     * @return \FOS\OAuthServerBundle\Model\ClientInterface The client.
      */
     protected function getClient(Request $request)
     {
