9aaab184b1ea60ad37abcaf5d096e0c5251d6a46
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2017-02-02 14:31:29 +0100

    Fix typo

--- Form/AuthSpaceType.php
+++ Form/AuthSpaceType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class AuthspaceType extends AbstractType
+class AuthSpaceType extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
